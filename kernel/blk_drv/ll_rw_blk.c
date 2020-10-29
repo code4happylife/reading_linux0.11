14d39a5ff2815611215506f0f243f5b1bbd534df
donngchao <1559273720@qq.com>
2020-10-29 10:22:14 +0800

    init block and memory

--- kernel/blk_drv/ll_rw_blk.c
+++ kernel/blk_drv/ll_rw_blk.c
@@ -159,7 +159,7 @@ void blk_dev_init(void)
 	int i;
 
 	for (i=0 ; i<NR_REQUEST ; i++) {
-		request[i].dev = -1;
+		request[i].dev = -1;//set the request to be free.
 		request[i].next = NULL;
 	}
 }
