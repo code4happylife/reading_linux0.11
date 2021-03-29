5b8efab33641888960d27dae2cb9061cc6171b07
marquisdong <marquisdong@tencent.com>
2021-03-29 20:42:42 +0800

    c practice

---
+++ testcase/20210329/calc.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  int a = 21;
+  int b = 10;
+  int c;
+  c = a+b;
+  printf("line 1: c value is:%d\n",c);
+  c = a-b;
+  printf("line 2: c value is:%d\n",c);
+}
